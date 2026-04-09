7ce68aedbe75c9425733aea34388cdaf310df0cc
firasj082 <firasj082@gmail.com>
2026-04-09 15:00:26 +0300

    Added Shape class, Circle, Rectangle subclasses and the main separately.

---
+++ Rectangle.java
@@ -0,0 +1,42 @@
+public class Rectangle extends Shape{
+    
+    private float width;
+    private float height;
+
+    public float getWidth(){
+
+        return this.width;
+    }
+
+    public void setWidth(float width){
+
+        this.width = width;
+    }
+
+    public float getHeight(){
+
+        return this.height;
+    }
+
+    public void setHeight(float height){
+
+        this.height = height;
+    }
+
+    @Override public float calculateArea(){
+
+        return getHeight() * getWidth();
+    }
+
+    public Rectangle(){
+
+        setWidth(0);
+        setHeight(0);
+    }
+
+    public Rectangle(float width, float height){
+
+        setWidth(width);
+        setHeight(height);
+    }
+}
---
+++ Shape.java
@@ -0,0 +1,7 @@
+public class Shape {
+    
+    public float calculateArea(){
+
+        return 0;
+    }
+}
