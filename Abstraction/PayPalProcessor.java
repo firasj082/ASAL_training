49c5d84ee315725f77fb2501da62a2e3bf13c5e3
firasj082 <firasj082@gmail.com>
2026-04-16 12:24:02 +0300

    The maven tasks

--- Abstraction/PayPalProcessor.java
+++
@@ -1,9 +0,0 @@
-package Abstraction;
-
-public class PayPalProcessor implements PaymentProcessor {
-    
-    public void process(double amount){
-        
-        System.out.println("The paypal implemented process.");
-    }
-}
